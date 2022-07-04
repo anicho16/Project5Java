9da45efb62f9e2c70b685fbd70537b8650326df2
Amber Nicholas <anicho16@my.westga.edu>
2022-07-04 09:10:11 -0500

    Removed all code from MancalaPane which is now in MancalaMenuBar and NewGamePane. Adjusted accessibility by creating accessors for panes.

--- src/edu/westga/cs6910/mancala/view/MancalaMenuBar.java
+++ src/edu/westga/cs6910/mancala/view/MancalaMenuBar.java
@@ -29,42 +29,42 @@ import javafx.event.EventHandler;
  *
  */
 public class MancalaMenuBar {
-	
+
 	private Game theGame;
 	private MancalaHelpDialog helpDialog;
 	private MancalaPane mainPane;
-	
-	public MancalaMenuBar(Game theGame) {
+
+	public MancalaMenuBar(Game theGame, MancalaPane mainPane) {
 		if (theGame == null) {
 			throw new IllegalArgumentException("Invalid Game object");
 		}
-		
+
 		this.theGame = theGame;
 		this.helpDialog = new MancalaHelpDialog();
 		this.mainPane = mainPane;
 	}
-	
+
 	public VBox createMenu() {
 		VBox vbxMenuHolder = new VBox();
-		
+
 		MenuBar mnuMain = new MenuBar();
-		
+
 		Menu mnuFile = this.createFileMenu();
-		
+
 		Menu mnuSettings = this.createStrategyMenu();
-				
+
 		mnuMain.getMenus().addAll(mnuFile, mnuSettings);
 		vbxMenuHolder.getChildren().addAll(mnuMain);
-		
+
 		return vbxMenuHolder;
 	}
 
 	private Menu createStrategyMenu() {
 		Menu mnuSettings = new Menu("_Computer Player");
 		mnuSettings.setMnemonicParsing(true);
-		
+
 		ToggleGroup tglStrategy = new ToggleGroup();
-		
+
 		RadioMenuItem mnuNear = new RadioMenuItem("N_ear");
 		mnuNear.setAccelerator(new KeyCodeCombination(KeyCode.E, KeyCombination.SHORTCUT_DOWN));
 		mnuNear.setOnAction(new EventHandler<ActionEvent>() {
@@ -73,10 +73,10 @@ public class MancalaMenuBar {
 				MancalaMenuBar.this.theGame.getComputerPlayer().setStrategy(new NearStrategy());
 			}
 		});
-		
+
 		mnuNear.setMnemonicParsing(true);
 		mnuNear.setToggleGroup(tglStrategy);
-		
+
 		RadioMenuItem mnuFar = new RadioMenuItem("F_ar");
 		mnuFar.setAccelerator(new KeyCodeCombination(KeyCode.A, KeyCombination.SHORTCUT_DOWN));
 		mnuFar.setOnAction(new EventHandler<ActionEvent>() {
@@ -87,7 +87,7 @@ public class MancalaMenuBar {
 		});
 		mnuFar.setMnemonicParsing(true);
 		mnuFar.setToggleGroup(tglStrategy);
-		
+
 		RadioMenuItem mnuRandom = new RadioMenuItem("_Random");
 		mnuRandom.setAccelerator(new KeyCodeCombination(KeyCode.R, KeyCombination.SHORTCUT_DOWN));
 		mnuRandom.setOnAction(new EventHandler<ActionEvent>() {
@@ -98,8 +98,8 @@ public class MancalaMenuBar {
 		});
 		mnuRandom.setMnemonicParsing(true);
 		mnuRandom.setToggleGroup(tglStrategy);
-		
-		SelectStrategy currentStrategy = this.theGame.getComputerPlayer().getStrategy();			
+
+		SelectStrategy currentStrategy = this.theGame.getComputerPlayer().getStrategy();
 		if (currentStrategy.getClass() == NearStrategy.class) {
 			mnuNear.setSelected(true);
 		} else if (currentStrategy.getClass() == RandomStrategy.class) {
@@ -115,7 +115,7 @@ public class MancalaMenuBar {
 	private Menu createFileMenu() {
 		Menu mnuFile = new Menu("_Game");
 		mnuFile.setMnemonicParsing(true);
-	
+
 		MenuItem mnuNew = new MenuItem("_New");
 		mnuNew.setMnemonicParsing(true);
 		mnuNew.setAccelerator(new KeyCodeCombination(KeyCode.N, KeyCombination.SHORTCUT_DOWN));
@@ -132,12 +132,12 @@ public class MancalaMenuBar {
 				}
 			}
 		});
-		
+
 		MenuItem mnuExit = new MenuItem("E_xit");
 		mnuExit.setMnemonicParsing(true);
 		mnuExit.setAccelerator(new KeyCodeCombination(KeyCode.X, KeyCombination.SHORTCUT_DOWN));
 		mnuExit.setOnAction(event -> System.exit(0));
-		
+
 		mnuFile.getItems().addAll(mnuNew, mnuExit);
 		return mnuFile;
 	}
--- src/edu/westga/cs6910/mancala/view/NewGamePane.java
+++ src/edu/westga/cs6910/mancala/view/NewGamePane.java
@@ -1,6 +1,7 @@
 /**
  * 
  */
+
 package edu.westga.cs6910.mancala.view;
 
 import edu.westga.cs6910.mancala.model.Game;
@@ -15,7 +16,9 @@ import javafx.scene.layout.GridPane;
 
 /**
  * Defines the panel in which the user selects which Player plays first.
- * @author amber
+ * 
+ * @author Amber Nicholasa
+ * @version 7.4.22
  *
  */
 public class NewGamePane extends GridPane {
@@ -23,40 +26,45 @@ public class NewGamePane extends GridPane {
 	private RadioButton radComputerPlayer;
 	private RadioButton radRandomPlayer;
 	private ComboBox<Integer> cmbGoalScore;
-	
+
 	private Game theGame;
 	private Player theHuman;
 	private Player theComputer;
 	private MancalaPane mainPane;
 
+	/**
+	 * Constructor for NewGamePane
+	 * @param theGame - represents current Game
+	 * @param mainPane - represents MancalaPane
+	 */
 	public NewGamePane(Game theGame, MancalaPane mainPane) {
 		this.theGame = theGame;
-		
+
 		this.theHuman = this.theGame.getHumanPlayer();
 		this.theComputer = this.theGame.getComputerPlayer();
 		this.mainPane = mainPane;
-		
+
 		this.buildPane();
 	}
-	
+
 	private void buildPane() {
 		this.setHgap(20);
-		
+
 		this.createNumberOfStonesChoice();
-		
-		this.createFirstPlayerItems();	
+
+		this.createFirstPlayerItems();
 	}
 
 	private void createFirstPlayerItems() {
-		this.radHumanPlayer = new RadioButton(this.theHuman.getName() + " first");	
+		this.radHumanPlayer = new RadioButton(this.theHuman.getName() + " first");
 		this.radHumanPlayer.setOnAction(new HumanFirstListener());
-		
+
 		this.radComputerPlayer = new RadioButton(this.theComputer.getName() + " first");
 		this.radComputerPlayer.setOnAction(new ComputerFirstListener());
-		
+
 		this.radRandomPlayer = new RadioButton("Random player first");
 		this.radRandomPlayer.setOnAction(new RandomFirstListener());
-		
+
 		ToggleGroup group = new ToggleGroup();
 		this.radHumanPlayer.setToggleGroup(group);
 		this.radComputerPlayer.setToggleGroup(group);
@@ -66,11 +74,11 @@ public class NewGamePane extends GridPane {
 		this.add(this.radComputerPlayer, 3, 0);
 		this.add(this.radRandomPlayer, 4, 0);
 	}
-	
+
 	private void createNumberOfStonesChoice() {
 		Label lblGoalScore = new Label("Starting Stones: ");
 		this.add(lblGoalScore, 0, 0);
-		
+
 		this.cmbGoalScore = new ComboBox<Integer>();
 		this.cmbGoalScore.getItems().addAll(1, 2, 3);
 		this.cmbGoalScore.setValue(1);
@@ -87,11 +95,11 @@ public class NewGamePane extends GridPane {
 	public boolean isHumanFirst() {
 		return this.radHumanPlayer.isSelected();
 	}
-	
+
 	public boolean isComputerFirst() {
 		return this.radComputerPlayer.isSelected();
 	}
-	
+
 	public boolean isRandomFirst() {
 		return this.radRandomPlayer.isSelected();
 	}
@@ -103,15 +111,15 @@ public class NewGamePane extends GridPane {
 			NewGamePane.this.theGame.startNewGame(NewGamePane.this.theGame.getComputerPlayer());
 		}
 	}
-	
-	/** 
+
+	/**
 	 * Defines the listener for computer player first button.
-	 */		
+	 */
 	private class RandomFirstListener implements EventHandler<ActionEvent> {
 		@Override
-		/** 
-		 * Enables the ComputerPlayerPanel and starts a new game. 
-		 * Event handler for a click in the computerPlayerButton.
+		/**
+		 * Enables the ComputerPlayerPanel and starts a new game. Event handler for a
+		 * click in the computerPlayerButton.
 		 */
 		public void handle(ActionEvent arg0) {
 			NewGamePane.this.setDisable(true);
@@ -119,15 +127,15 @@ public class NewGamePane extends GridPane {
 			NewGamePane.this.chooseRandomPlayer();
 		}
 	}
-	
-	/** 
+
+	/**
 	 * Defines the listener for computer player first button.
-	 */		
+	 */
 	private class ComputerFirstListener implements EventHandler<ActionEvent> {
 		@Override
-		/** 
-		 * Enables the ComputerPlayerPanel and starts a new game. 
-		 * Event handler for a click in the computerPlayerButton.
+		/**
+		 * Enables the ComputerPlayerPanel and starts a new game. Event handler for a
+		 * click in the computerPlayerButton.
 		 */
 		public void handle(ActionEvent arg0) {
 			NewGamePane.this.mainPane.getPnComputerPlayer().setDisable(false);
@@ -136,14 +144,14 @@ public class NewGamePane extends GridPane {
 		}
 	}
 
-	/** 
+	/**
 	 * Defines the listener for human player first button.
-	 */	
+	 */
 	private class HumanFirstListener implements EventHandler<ActionEvent> {
 		@Override
-		/** 
-		 * Sets up user interface and starts a new game. 
-		 * Event handler for a click in the human player button.
+		/**
+		 * Sets up user interface and starts a new game. Event handler for a click in
+		 * the human player button.
 		 */
 		public void handle(ActionEvent event) {
 			NewGamePane.this.setDisable(true);
